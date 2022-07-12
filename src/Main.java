bad7859885cbfcb885191feaf77486da10dfe6b2
KaTatiana <badinat@mail.ru>
2022-07-12 08:28:38 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,24 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Задача 1
+        //task 1
         byte num =0;
-        short num6 = 10;
+        short num6 = 6;
         int num1=1;
         long num2=2L;
         float num4=4.4f;
         double num5=5.5;
 
-        //Задача 2
-        System.out.println("Hello world!");
+        //task 2
+        float boxerWeight1 = 78.2f;
+        float boxerWeight2 = 82.7f;
+
+        System.out.println("общий вес двух бойцов:"+(boxerWeight1+boxerWeight2));
+        System.out.println("разница между весами бойцов:" +boxerWeight1%boxerWeight2);
+
+        //task 3
+
+
+
     }
 }
