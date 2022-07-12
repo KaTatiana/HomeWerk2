235653afc2bb6ea522f28824bb89b5fb6c97326a
KaTatiana <badinat@mail.ru>
2022-07-12 08:11:20 +0300

    1 task

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+
+        //Задача 1
+        byte num =0;
+        short num6 = 10;
+        int num1=1;
+        long num2=2L;
+        float num4=4.4f;
+        double num5=5.5;
+
+        //Задача 2
         System.out.println("Hello world!");
     }
 }
